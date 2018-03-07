52dde7d9677cb92cc6568c938ddcff95f530ef93
zacharyzadoo <zzadoo@horizon.csueastbay.edu>
2018-03-06 17:34:13 -0800

    start

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
-var counter = document.getElementById('counter');
-var fromLeft = window.pageYOffset;
-
-counter.innerHTML = "counter text";
+var car = document.getElementById('car');
+var fromLeft = window.pageXOffset;
 
 //when user is scrolling update the var fromTop and make decisions to change things on the page
 window.onscroll = function() {
   //when the user scrolls do all this...
   fromLeft = window.pageXOffset;
-  //test
-  console.log(fromLeft);
 
-  counter.style.left = 100 + fromLeft + "px";
-  if (fromLeft > 1000) {
-    counter.innerHTML = "more than 1000 leagues.";
-    counter.style.background =  "green";
+  car.style.left = 200 + fromLeft + "px";
+  console.log(fromLeft);
+  if (fromLeft >= 0) {
+    car.style.backgroundImage = "url(img/car.png)";
   } else {
-    counter.innerHTML = "counter text";
-    counter.style.background = "blue";
+    car.style.backgroundImage = "url(img/car.png)";
   }
 }
