c79fb5e4f608a2289a236da6d6a4feae4605a1da
Zachary <zzadoo@horizon.csueastbay.edu>
2018-03-12 22:11:55 -0700

    up

--- main.js
+++ main.js
@@ -6,24 +6,11 @@ window.onscroll = function() {
   //when the user scrolls do all this...
   fromLeft = window.pageXOffset;
 
-<<<<<<< HEAD
-  //Keeps the block 100 px from the left
-  counter.style.left = 100 + fromLeft + "px";
-
-  // if (fromLeft > 1000) {
-  //   counter.innerHTML = "more than 1000 leagues.";
-  //   counter.style.background =  "green";
-  // } else {
-  //   counter.innerHTML = "counter text";
-  //   counter.style.background = "blue";
-  // }
-=======
-  car.style.left = 200 + fromLeft + "px";
-  console.log(fromLeft);
-  if (fromLeft >= 0) {
+  car.style.left = 500 + fromLeft + "px";
+  console.log(fromLeft); //remove later
+  if (fromLeft > 0) {
     car.style.backgroundImage = "url(img/car.png)";
   } else {
     car.style.backgroundImage = "url(img/car.png)";
   }
->>>>>>> 52dde7d9677cb92cc6568c938ddcff95f530ef93
 }
